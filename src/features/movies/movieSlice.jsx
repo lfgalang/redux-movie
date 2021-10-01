b8aca430058619bf315be618bd40413736a3df2e
lfgalang <galanpipe3@gmail.com>
2021-10-01 11:34:00 -0500

    added an async action creator

--- src/features/movies/movieSlice.jsx
+++ src/features/movies/movieSlice.jsx
@@ -1,4 +1,17 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import MovieApi from '../../common/apis/MovieApi.jsx'
+import { APIKey } from '../../common/apis/MovieApiKey.jsx'
+
+// to use asyncronus action creators createAsyncThunk
+export const fetchAsyncMovies = createAsyncThunk('movies/fetchAsyncMovies', async () => {
+    
+    const movieText = "Matrix"
+
+    const response = await MovieApi.get(
+        `?apikey=${APIKey}&s=${movieText}&type=movie`
+    );
+    return response.data
+})
 
 const initialState = {
     movies: [],
@@ -12,6 +25,18 @@ const movieSlice = createSlice({
             state.movies = payload;
         }
     },
+    extraReducers: {
+        [fetchAsyncMovies.pending]: () => {
+            console.log("pending")
+        },
+        [fetchAsyncMovies.fulfilled]: (state, {payload}) => {
+            console.log("fetched succesfully")
+            return {...state, movies: payload}
+        },
+        [fetchAsyncMovies.rejectede]: (state, {payload}) => {
+            console.log("Rejected")
+        }
+    }
 })
 
 export const { addMovies } = movieSlice.actions;
